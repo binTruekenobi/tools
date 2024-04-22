b6cd64d8b330597e0d5598499f43e1fb145d3af0
binTruekenobi <162815260+binTruekenobi@users.noreply.github.com>
2024-04-22 11:28:19 +0100

    Add files via upload

---
+++ tools/plot_matrices.py
@@ -0,0 +1,16 @@
+import vectors_tool as vt
+import plot_functions as pf
+
+"""
+plots v^T M v for v taking values with the x and y ranges
+"""
+
+matrix = [[1, 1], [1, 1]]
+vec = vt.matrix_vecmult(matrix)
+def function(x, y):
+    return vec.multv([x, y])
+
+def plot_matrix(Lx, Ux, Sx, Ly=None, Uy=None, Sy=None):
+    plot_function_3d(function, Lx, Ux, Sx, Ly=Ly, Uy=Uy, stepsy=Sy)
+
+plot_matrix(-5, 5, 100)
